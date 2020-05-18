ba9462e6fa55c0aa307743f50307d5f84f4a6f1e
Maxim Yefremov <maxYefr@gmail.com>
2020-05-18 21:54:15 +0300

    long and fast query

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/ypapax/cancel-postgres-query-go-pg
+
+go 1.13
+
+require (
+	github.com/go-pg/pg v8.0.6+incompatible // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/lib/pq v1.5.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.6.0 // indirect
+	mellium.im/sasl v0.2.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,177 @@
+package main
+
+import (
+	"context"
+	"github.com/labstack/echo"
+	"os"
+	"time"
+
+	"github.com/go-pg/pg"
+	_ "github.com/lib/pq" // postgres driver
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+)
+
+const port = "7999"
+
+var db *pg.DB
+
+func main() {
+	Prepare(logrus.TraceLevel)
+	const connStrEnv = "POSTGRESQL_ADDRESS"
+	connectionString := os.Getenv(connStrEnv)
+	if len(connectionString) == 0 {
+		logrus.Fatalf("missing env var '%+v'", connStrEnv)
+	}
+	var err error
+	db, err = ConnectToPostgresTimeout(connectionString, 5*time.Second, time.Second)
+	if err != nil {
+		logrus.Fatalf("%+v", err)
+	}
+	logrus.Infof("connected to db %+v", connectionString)
+
+	go server()
+
+	cont, cancel := context.WithCancel(context.Background())
+	go func() {
+		sl := 17 * time.Second
+		logrus.Infof("waiting for %s before canceling", sl)
+		time.Sleep(sl)
+		logrus.Infof("canceling")
+		cancel()
+	}()
+	c, err := queryLong(cont, db)
+	if err != nil {
+		logrus.Errorf("%+v", err)
+	}
+	logrus.Infof("count: %+v", c)
+	select {}
+}
+
+type Filing struct {
+	Id int `sql: "id"`
+}
+
+func queryLong(cont context.Context, db *pg.DB) (int, error) {
+	var c []Filing
+	count, err := db.ModelContext(cont, &c).Count()
+	if err != nil {
+		return 0, errors.WithStack(err)
+	}
+	logrus.Infof("request is done: %+v", count)
+	return count, nil
+}
+
+func queryFast(cont context.Context, db *pg.DB) (int, error) {
+	var c Filing
+	err := db.ModelContext(cont, &c).First()
+	if err != nil {
+		return 0, errors.WithStack(err)
+	}
+	logrus.Infof("request is done: %+v", c)
+	return c.Id, nil
+}
+
+// ConnectToPostgres connects to postgres instance
+func ConnectToPostgres(connectionString string) (*pg.DB, error) {
+	if connectionString == "" {
+		return nil, errors.Errorf("missing connectionString")
+	}
+	opt, err := pg.ParseURL(connectionString)
+	if err != nil {
+		return nil, errors.Wrap(err, "connecting to postgres with connection string: "+connectionString)
+	}
+
+	//opt.MaxConnAge
+
+	db := pg.Connect(opt)
+	_, err = db.Exec("SELECT 1")
+	if err != nil {
+		err = errors.WithStack(err)
+		return nil, err
+	}
+
+	return db, nil
+}
+
+// ConnectToPostgres connects to postgres instance
+func ConnectToPostgresTimeout(connectionString string, timeout, retry time.Duration) (*pg.DB, error) {
+	var (
+		connectionError error
+		db              *pg.DB
+	)
+	connected := make(chan bool)
+	giveUp := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-giveUp:
+				logrus.Tracef("giving up")
+				return
+			default:
+			}
+			db, connectionError = ConnectToPostgres(connectionString)
+			if connectionError != nil {
+				time.Sleep(retry)
+				continue
+			}
+			connected <- true
+			break
+		}
+	}()
+	select {
+	case <-time.After(timeout):
+		err := errors.Wrapf(connectionError, "timeout %s connecting to db", timeout)
+		return nil, err
+	case <-connected:
+		close(giveUp)
+	}
+	return db, nil
+}
+
+func Prepare(logLevel logrus.Level) {
+	customFormatter := logrus.TextFormatter{
+		ForceColors:   true,
+		FullTimestamp: true,
+	}
+	customFormatter.TimestampFormat = "2006-01-02 15:04:05.999999999 -0700"
+	logrus.SetFormatter(&customFormatter)
+	logrus.SetReportCaller(true)
+	logrus.SetLevel(logLevel)
+}
+
+func server() {
+	e := echo.New()
+	e.GET("/long", handlerLong)
+	e.GET("/fast", handlerFast)
+	logrus.Tracef("listening %+v", port)
+	e.Logger.Fatal(e.Start(":" + port))
+
+}
+
+func handlerLong(c echo.Context) error {
+	logrus.Tracef("hello handlerLong started")
+	t1 := time.Now()
+	count, err := queryLong(c.Request().Context(), db)
+	if err != nil {
+		logrus.Errorf("%+v", err)
+	}
+	if err := c.JSON(200, map[string]interface{}{"count": count, "time": time.Since(t1)}); err != nil {
+		logrus.Errorf("error: %+v", err)
+		return err
+	}
+	return nil
+}
+
+func handlerFast(c echo.Context) error {
+	logrus.Tracef("hello handlerFast started")
+	count, err := queryFast(c.Request().Context(), db)
+	if err != nil {
+		logrus.Errorf("%+v", err)
+	}
+	if err := c.JSON(200, map[string]int{"id": count}); err != nil {
+		logrus.Errorf("error: %+v", err)
+		return err
+	}
+	return nil
+}
